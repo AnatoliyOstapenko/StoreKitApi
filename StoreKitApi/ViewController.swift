08c6686c35458a29f901002e88e98cb47fb472f5
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-09-14 20:17:17 +0300

    Initial Commit

---
+++ StoreKitApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreKitApi
+//
+//  Created by MacBook on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
